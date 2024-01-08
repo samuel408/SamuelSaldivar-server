dccf8f572dec6efad2d9e15439442d095fe61550
Samuel408 <samuelsaldivar11@gmail.com>
2024-01-08 15:24:03 -0800

    fixing cors configuration

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const nodemailer = require('nodemailer');
 const app = express();
 const port =  process.env.PORT || 3001;
 
-const corsOptions = {
-    origin: "https://main--lively-hamster-bbba82.netlify.app/",
-  };
-  
-  app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://main--lively-hamster-bbba82.netlify.app',
+  }));
   app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
