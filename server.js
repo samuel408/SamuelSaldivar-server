d1e7cec5bb4b63cf5b459949d6d086f627e84b32
Samuel408 <samuelsaldivar11@gmail.com>
2024-01-17 01:19:55 -0800

    incorporated Openai API

--- server.js
+++ server.js
@@ -2,16 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-
+const OpenAI = require("openai");
 const app = express();
 const port =  process.env.PORT || 3001;
-
+app.use(bodyParser.json());
 app.use(cors({
   origin: '*',
   }));
 
-app.use(bodyParser.json());
+// open ai 
 
+const openai = new OpenAI({
+  apiKey: "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",
+});
+app.post('/chat', async (req, res) => {
+  const prompt = "you need to pitch samuel to this potential employer whatever the employer asks relate it to his resume " + req.body;
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "system", content: prompt }],
+    max_tokens: 150,
+    temperature: 0,
+  },);
+  console.log(completion.choices[0].message.content);
+  res.send(completion.choices[0].message.content);
+
+});
+
+//contact form
 app.post("/", (req, res) => {
     console.log('Received a POST request to / from:', req.get('origin'));
 
@@ -42,6 +60,7 @@ app.post("/", (req, res) => {
 
   res.json({ message: 'Form submitted successfully' });
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
